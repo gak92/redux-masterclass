0e399db8d88474bb415fe428dbd839bf552f6950
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-11 21:04:20 +0500

    used redux thunk as middleware to initialize amount

--- ch1/index.js
+++ ch1/index.js
@@ -1,39 +1,53 @@
+import axios from "axios";
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 // action name constant (Type)
+const INIT = 'initialize';
 const INC = 'increment';
 const DEC = 'decrement';
 const INCBYAMOUNT = 'incrementByAmount';
 
 // store
-const store = createStore(reducer, applyMiddleware(logger.default));
+const store = createStore(reducer, applyMiddleware(logger.default, thunk.default));
 
 const history = [];
 
 // reducer
 function reducer(state={amount:1}, action) {
-  if(action.type === INC) {
-    return {amount: state.amount + 1};
+  switch(action.type) {
+    case INIT:
+      return {amount: action.payload};
+    case INC:
+      return {amount: state.amount + 1};
+    case DEC:
+      return {amount: state.amount - 1};
+    case INCBYAMOUNT:
+      return {amount: state.amount + action.payload};
+    default:
+      return state;
   }
-
-  if(action.type === DEC) {
-    return {amount: state.amount - 1};
-  }
-
-  if(action.type === INCBYAMOUNT) {
-    return {amount: state.amount + action.payload};
-  }
-
-  return state;
 }
 
 // global state
 // store.subscribe(() => {
 //   history.push(store.getState());
 //   console.log(history);
-
 // });
+
+// Async call
+async function getUser() {
+  const { data } = await axios.get('http://localhost:3000/accounts/1');
+  console.log(data);
+}
+// getUser();
+
+// Action Creator
+async function initialize(dispatch, getState) {
+  const { data } = await axios.get('http://localhost:3000/accounts/1');
+  dispatch({type: INIT, payload: data.amount});
+}
 
 function increment() {
   return {type: INC};
@@ -43,14 +57,13 @@ function decrement() {
   return {type: DEC};
 }
 
-function incrementByAmount() {
-  return {type: INCBYAMOUNT, payload: 4}
+function incrementByAmount(val) {
+  return {type: INCBYAMOUNT, payload: val}
 }
 
 // dispatch an action
-setInterval(() => {
-  store.dispatch(incrementByAmount());
-
+setTimeout(() => {
+  store.dispatch(initialize);
 }, 2000);
 
 // console.log(store.getState());
