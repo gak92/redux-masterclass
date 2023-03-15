d9939bdae79aa8555e85b1698c835d248c98fff7
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-15 12:27:53 +0500

    create products component and fetch all products from the db.json

---
+++ redux-api-example/src/features/products/Products.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProductsAsync } from "./productsSlice";
+import "./Products.css";
+
+export function Products() {
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.products.products);
+
+  return (
+    <div>
+      <div className="row">
+        <button
+          className="button"
+          aria-label="Decrement value"
+          onClick={() => dispatch(fetchProductsAsync())}
+        >
+          Get Products
+        </button>
+
+      </div>
+
+      <div>
+        {
+          products.map((product) => (
+            <div className="card">
+              <img src={product.thumbnail} alt={product.title} style={{width: '100%'}} />
+              <h1>{product.title}</h1>
+              <p className="price">{product.price}</p>
+              <p>{product.description}</p>
+              <p><button>Add to Cart</button></p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
